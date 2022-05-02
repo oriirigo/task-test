5731950de1773b647c7d49c2bae4aed9fa76385b
[oriirigo] <[orii.irigo@gmail.com]>
2022-05-02 07:43:40 -0300

    Fixed controlled components

--- components/TodoList.js
+++ components/TodoList.js
@@ -4,28 +4,28 @@ import { useTasks } from "../context/taskContext"
 import { db } from "../firebase/firebase"
 import Todo from './Todo'
 import Styles from "../styles/StylesFirst.module.css"
-import { useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 
 export default function TodoList() {
   const [tasks, setTasks] = useState([])
-  const { setStatus, setTitle, status, title, setIsUpDate, isUpDate, id, setId } = useTasks()
- 
+  const { setStatus, setTitle, status, title, setIsUpDate, isUpDate, id, setId, statusIn } = useTasks()
 
-    // form validation rules 
-    const validationSchema = Yup.object().shape({
-      title: Yup.string()
-          .required('Status is required'),
-      status: Yup.string()
-          .required('Status is required'),
-      
+
+  // form validation rules 
+  const validationSchema = Yup.object().shape({
+    title: Yup.string()
+      .required('Status is required'),
+    status: Yup.string()
+      .required('Status is required'),
+
   });
   const formOptions = { resolver: yupResolver(validationSchema) };
 
-    // get functions to build form with useForm() hook
-    const { register, handleSubmit, reset, formState } = useForm(formOptions);
-    const { errors } = formState;
+  // get functions to build form with useForm() hook
+  const { register, handleSubmit, reset, formState } = useForm(formOptions);
+  const { errors } = formState;
 
 
 
@@ -39,13 +39,13 @@ export default function TodoList() {
         data: doc.data()
       })))
     })
- 
- 
+
+
   }, [])
- 
- 
+
+
   const onSubmit = async (e) => {
- 
+
     try {
       await addDoc(collection(db, 'task'), {
         title: title,
@@ -58,7 +58,7 @@ export default function TodoList() {
       alert(err)
     }
   }
- 
+
   const upDateFields = async (e) => {
 
     let fieldToEdit = doc(db, 'task', id)
@@ -76,61 +76,53 @@ export default function TodoList() {
         console.log(err)
       })
   }
- 
- 
+
+
   return (
     <>
-    <header className={Styles.header}>
+      <header className={Styles.header}>
         <br></br><h2 className={Styles.header1}>TODO LIST</h2></header>
       <div className={Styles.mainDiv}>
-        
         <div className={Styles.id}>  <br></br>
-            <h3 className={Styles.h3}>TODO LIST</h3></div>
+          <h3 className={Styles.h3}>TODO LIST</h3></div>
         <div >
-        <form className={Styles.form}   >
+          <form className={Styles.form}   >
             <div  >
-        <input className={Styles.input} aling='right' placeholder='New Todo' type='text' value={title} name='status'{...register('title') }  onChange={e => setTitle(e.target.value)} />
-        {errors.title && errors.title.type=='required' && <p>Title is required</p>}
-        </div>
-       
-       <div ><br></br><br></br></div>
-    
-        <div >
-           
-          <select  className={Styles.input} name='status'{...register('status') } onChange={e => setStatus(e.target.value)} >
-            <option disabled    >Status(Pending/ In progress/ Done</option>
-            <option value="Done" name="Done">Done</option>
-            <option value="In progress" name="In progress" >In progress</option>
-            <option value="Pending" name="Pending" >Pending</option>
-            
-          </select>
-          <div>{errors.title?.message}</div>
-        </div>
-        <div >
-            <br></br>
-            <br></br>
-        {!isUpDate ? (<button className={Styles.button} onClick={handleSubmit(onSubmit)}>ADD</button>) :
-          (<button onClick={handleSubmit(upDateFields)}  className={Styles.button} >MODIFY</button>)}
-          </div>
+              <input className={Styles.input} aling='right' placeholder='New Todo' type='text' value={title} name='status'{...register('title')} onChange={e => setTitle(e.target.value)} />
+              {errors.title && errors.title.type == 'required' && <p>Title is required</p>}
+            </div>
+            <div ><br></br><br></br></div>
+            <div >
+              <select className={Styles.input} name='status'{...register('status')} onChange={e => setStatus(e.target.value)} >
+                <option disabled selected>Status(Pending/ In progress/ Done)</option>
+                {statusIn &&
+                  statusIn.map((e) => (
+                    <option key={e.id} value={e.status}>
+                      {" "}
+                      {e.status}{" "}
+                    </option>
+                  ))}
+              </select>
+              <div>{errors.title?.message}</div>
+            </div>
+            <div >
+              <br></br>
+              <br></br>
+              {!isUpDate ? (<button className={Styles.button} onClick={handleSubmit(onSubmit)}>ADD</button>) :
+                (<button onClick={handleSubmit(upDateFields)} className={Styles.button} >MODIFY</button>)}
+            </div>
           </form>
- </div>
-
-     
+        </div>
         {tasks.map((task) => (
           <Todo
- 
             id={task.id}
             key={task.id}
             title={task.data.title}
             status={task.data.status}
- 
           />
         ))}
-
- 
-      
       </div>
     </>
   )
 }
- 
+
--- context/taskContext.js
+++ context/taskContext.js
@@ -11,8 +11,13 @@ export const TaskProvider = ({ children }) => {
     const [id, setId] = useState()
     const [isUpDate, setIsUpDate] = useState(false)
 
+    const statusIn=[
+        {status:"Done", id:1},
+        {status:"In progress",id:2},
+        {status:"Pending",id:3}
+    ]
     return (
-        <TaskContext.Provider value={{ title, status, setStatus, setTitle, isUpDate, setIsUpDate, id, setId }}>
+        <TaskContext.Provider value={{ title, status, setStatus, setTitle, isUpDate, setIsUpDate, id, setId,statusIn }}>
             {children}
         </TaskContext.Provider>
     )
